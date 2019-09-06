1ef09eab2718356e333d8fe31d4f681f4727f81d
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-09-06 16:33:17 +0530

    updated files

--- SmartWatchApplication.js
+++ SmartWatchApplication.js
@@ -1,22 +1,44 @@
+
+
+
 $(document).ready(function(){
     $(".mainDiv").show();
     $(".messageDiv").hide();
     $(".musicDiv").hide();
     $(".timeDiv").hide();
+    
+        var today = new Date();
+        var time1 = (
+        today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        )
+        document.getElementById("currentTime").innerHTML = time1;
         
     $("#music").click(function(){
     $(".musicDiv").show();
       $(".mainDiv").hide();
       $(".mainDiv").hide();
       $(".timeDiv").hide();
+      $(".sms2").hide();
+      
     });
        
       $("#message").click(function(){
         $(".mainDiv").hide();
-        $(".messageDiv").show();
+       if($(".messageDiv").show())
+       {
+        const msg = [
+            {  sms: "hii..."},
+            {  sms: "where are u??"},
+            {  sms: "I am in benglore"},
+            {  sms: "ok..."},
+            {  sms: "Come ASAP.."}
+        
+        ];
+        document.getElementById("sms").innerHTML=msg.sms;
+      
+       };
         $(".musicDiv").hide();
         $(".timeDiv").hide();
-  
     });
     $("#time").click(function(){
         $(".mainDiv").hide();
@@ -25,5 +47,6 @@ $(document).ready(function(){
           $(".musicDiv").hide();
         
       });
-   
   });
+  
+
